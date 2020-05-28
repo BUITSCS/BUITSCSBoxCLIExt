8b76afc1ee5527d74905722b9f90763bce6ce4bd
Brad <Brad_Hodges@baylor.edu>
2020-05-28 09:35:15 -0500

    folder and trash listing for users

--- BoxCliExt.ps1
+++ BoxCliExt.ps1
@@ -1,9 +1,6 @@
 #Powershell module to extend functionality of the Box CLI interface for reporting and admin functions.
 #Brad Hodges 2020
 
-#Dev values
-$UserBearID = "Brad_hodges"
-
 #Check for functional Box CLI
 
 #Functions to do:
@@ -42,6 +39,7 @@ Function Get-BoxUserID {
     Param (
         [Parameter(Mandatory=$true,Position=0)]
         [string]$UserBearID)
+    Write-Debug "Get-BoxUserID $UserBearID"
     $BoxUser = box users --filter=$UserBearID --json | ConvertFrom-Json
     $BoxUserID = $BoxUser."id"
     Return $BoxUserID
@@ -54,8 +52,10 @@ Function Get-BoxFolderList {
         [string]$BoxUserID,
         [Parameter(Mandatory=$true, Position=1)]
         [string]$BoxFolderID)
+    Write-Debug "Get-BoxFolderList $BoxUserID $BoxFolderID"
     $BoxUserFolders = box folders:get $BoxFolderID --as-user=$BoxUserID --json | ConvertFrom-Json
     $BoxUserFolderList = $BoxUserFolders."item_collection"."entries"
+    Write-Debug $BoxUserFolderList[3]
     Return $BoxUserFolderList
 }
 
@@ -66,6 +66,7 @@ Function Get-BoxUserFolderCollaboration {
         [string]$BoxUserID, 
         [Parameter(Mandatory=$true,Position=1)]
         [string]$BoxFolderID)
+    Write-Debug "Get-BoxUserFolderCollaboration $BoxUserID $BoxFolderID"
     $BoxFolderCollaboration = box folders:collaborations $BoxFolderID --as-user=$BoxUserID --json | ConvertFrom-Json
     Return $BoxFolderCollaboration
 }
@@ -74,65 +75,182 @@ Function Get-BoxUserRootFolders {
     Param (
         [Parameter(Mandatory=$true,Position=0)]
         [string]$BoxUserID)
+    Write-Debug "Get-BoxUserRootFolders $BoxUserID"
     $BoxFolders = Get-BoxFolderList -BoxUserID $BoxUserID -BoxFolderID "0"
     Return $BoxFolders
 }
 
-Function Get-BoxUserOwnedFolders {
+#Return folder names and collaboration levels
+#TODO: Add loose files
+Function Get-BoxUserFolders {
     Param (
     [Parameter(Mandatory=$true,Position=0)]
-    [string]$UserBearID)
-    $BoxUserID = Get-BoxUserID -UserBearID $UserBearID
+    [string]$UserBearID,
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$BoxUserID)
+    Write-Debug "Get-BoxUserFolders $UserBearID $BoxUserID"
     $BoxFolders = Get-BoxUserRootFolders -BoxUserID $BoxUserID
-    $i = 0
     $BoxFoldersOutputList = @()
+    $BoxFolderCollaboratorList = @()
+    $BoxUserFoundInList = $False
+    $UserEmail = $UserBearID + $EmailDomain
+    $BoxUserFolderCollabLevel = ""
     $BoxFolderOutput = [PSCustomObject]@{
-        FolderName = ""
+        ItemName = ""
+        Type = ""
         ID = ""
-        Owner = ""
+        CollabLevel = ""
+        Owner = $False
     }
     Foreach ($BoxFolder in $BoxFolders) {
-        #$BoxFolderID = $BoxFolder.id
-        #$BoxFolder.name
-        $BoxFolderOutput.FolderName = $BoxFolder.name
-        $BoxFolderOutput.ID = $BoxFolder.id
-        $BoxFolderOutput.Owner = $UserBearID + "@baylor.edu"
-        #Create array of objects that are this user's owned folders
-        $BoxFolderCollaborations = Get-BoxUserFolderCollaboration -BoxUserID $BoxUserID -BoxFolderID $BoxFolder.id
-        foreach ($BoxFolderCollaboration in $BoxFolderCollaborations) {
-            $BoxFolderCollaboration.accessible_by.login
-            $BoxFolderCollaboration.role
-            if ($BoxFolderCollaboration.accessible_by.login -eq ($UserBearID + "@baylor.edu")) {
-                $BoxFolderOutput.Owner = ""
+        if ($BoxFolder.type -eq "folder") {
+            $BoxFolderID = $BoxFolder.id
+            $BoxFolderCollaborations = Get-BoxUserFolderCollaboration -BoxUserID $BoxUserID -BoxFolderID $BoxFolderID
+            $BoxUserFoundInList = $False
+            $BoxUserFolderCollabLevel = ""
+            $BoxFolderOutput = [PSCustomObject]@{
+                Name = ""
+                Type = ""
+                ID = ""
+                CollabLevel = ""
+                Owner = $False
             }
+            Write-Debug $BoxFolder.name
+            #If the collaborations list is null, this is a folder owned by the user that isn't shared.
+            if ($null -eq $BoxFolderCollaborations) {
+                $BoxFolderOutput.Name = $BoxFolder.name
+                $BoxFolderOutput.ID = $BoxFolder.id
+                $BoxFolderOutput.Type = "folder"
+                $BoxFolderOutput.CollabLevel = "owner"
+                $BoxFolderOutput.Owner = $True
+                $BoxUserFolderCollabLevel = "owner"
+                Write-Debug "Null Collaboration list - non-shared folder: $BoxFolderOutput"
+            }
+            #If the collaborations list isn't null, check if the user is the owner
+            else {
+                #Go through each collaboration
+                foreach ($BoxFolderCollaboration in $BoxFolderCollaborations) {
+                    #Go through each collaborator
+                    foreach ($BoxFolderCollaborator in $BoxFolderCollaboration) {
+                        if ($BoxFolderCollaborator.accessible_by.login -eq $UserEmail) {
+                            $BoxUserFoundInList = $True
+                            $BoxUserFolderCollabLevel = $BoxFolderCollaborator.role
+                        }
+                    }
+                }
+                #If this user isn't found in the list, this user is the owner
+                if (!$BoxUserFoundInList) {
+                    $BoxFolderOutput.Name = $BoxFolder.name
+                    $BoxFolderOutput.ID = $BoxFolder.id
+                    $BoxFolderOutput.Type = "folder"
+                    $BoxFolderOutput.CollabLevel = "owner"
+                    $BoxFolderOutput.Owner = $True
+                    Write-Debug "User isn't found in the list of collaborators for this folder, so they're the owner: $BoxFolderOutput"
+                }
+                #If this user is found in the list, record their collaboration level
+                else {
+                    $BoxFolderOutput.Name = $BoxFolder.name
+                    $BoxFolderOutput.ID = $BoxFolder.id
+                    $BoxFolderOutput.Type = "folder"
+                    $BoxFolderOutput.Owner = $False
+                    $BoxFolderOutput.CollabLevel = $BoxUserFolderCollabLevel
+                    Write-Debug "User found in the list of collaborators for this folder, so they're an invited collaborator: $BoxFolderOutput"
+                }
+            }
+            $BoxFoldersOutputList += $BoxFolderOutput
         }
-        $BoxFoldersOutputList += $BoxFolderOutput
-        $i += 1
     }
-    Return $BoxFolderOutputList
+    Return $BoxFoldersOutputList
 }
 
-$BoxUserID = Get-BoxUserID -UserBearID $UserBearID
-#$BoxFolders = Get-BoxUserRootFolders -BoxUserID $BoxUserID
-$BoxFolderOutputList = Get-BoxUserOwnedFolders -UserBearID $UserBearID
-$BoxFolderOutputList
-
-<#
-Foreach ($BoxFolder in $BoxFolders) {
-    $BoxFolderID = $BoxFolder.id
-    $BoxFolderCollaboration = Get-BoxUserFolderCollaboration -BoxUserID $BoxUserID -BoxFolderID $BoxFolderID
-    $BoxFolderCollaboration
+#Return list of an item in a user's trash
+Function Get-BoxUserTrashItemDetails {
+    Param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]$BoxUserID,
+        [Parameter(Mandatory=$true,Position=1)]
+        [string]$BoxTrashID,
+        [Parameter(Mandatory=$true,Position=2)]
+        [string]$BoxTrashedItemType)
+        Write-Debug "Get-BoxUserTrashItemDetails $BoxUserID $BoxTrashID $BoxTrashedItemType"
+        $BoxTrashedItem = box trash:get $BoxTrashedItemType $BoxTrashID --as-user=$BoxUserID --json | ConvertFrom-Json
+        Return $BoxTrashedItem
 }
 
-<#
-$BoxUserFolder = $BoxUserFolderList[3]
-#foreach ($BoxUserFolder in $BoxUserFolderList) {
-    $BoxUserFolderID = $BoxUserFolder."id"
-    $BoxUserFolderID
-    $BoxUserFolderName = $BoxUserFolder."name"
-    $BoxUserFolderName
-    $BoxUserFolderCollaborations = box folders:collaborations $BoxUserFolderID --as-user=$BoxUserID --json | ConvertFrom-Json
-    $BoxUserFolderCollaborations
-#}
+#Return list of user's trash contents
+Function Get-BoxUserTrashList {
+    Param (
+    [Parameter(Mandatory=$true,Position=0)]
+    [string]$BoxUserID,
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$UserBearID)
+    Write-Debug "Get-BoxUserTrashList $BoxUserID"
+    $UserTrashList = box trash --as-user=$BoxUserID --json | ConvertFrom-Json
+    $UserEmail = $UserBearID + $EmailDomain
+    $TrashListOutput = @()
+    $TrashItemOutput = [PSCustomObject]@{
+        Name = ""
+        Owner = ""
+        Type = ""
+        ID = ""
+        Size = ""
+        FolderItems = ""
+        ParentID = ""
+        ParentName = ""
+        TrashedDate = ""
+    }
+    foreach ($TrashItem in $UserTrashList) {
+        #Write-Debug $TrashItem
+        $TrashItemOutput = [PSCustomObject]@{
+            Name = ""
+            Owner = ""
+            Type = ""
+            ID = ""
+            Size = ""
+            ParentID = ""
+            ParentName = ""
+            TrashedDate = ""
+        }
+        $TrashItemDetails = Get-BoxUserTrashItemDetails -BoxUserID $BoxUserID -BoxTrashID $TrashItem."id" -BoxTrashedItemType $TrashItem."type"
+        #Write-Debug $TrashItemDetails
+        #Write-Debug $TrashItemDetails.owned_by.login
+        $TrashItemOutput.Name = $TrashItemDetails.name
+        $TrashItemOutput.Owner = $TrashItemDetails.owned_by.login
+        $TrashItemOutput.Type = $TrashItemDetails.type
+        $TrashItemOutput.ID = $TrashItemDetails.id
+        $TrashItemOutput.Size = $TrashItemDetails.size
+        $TrashItemOutput.TrashedDate = $TrashItemDetails.trashed_at
+        if ($null -ne $TrashItemDetails.parent) {
+            Write-Debug $TrashItemDetails.parent
+            $TrashItemOutput.ParentID = $TrashItemDetails.parent.id
+            $TrashItemOutput.ParentName = $TrashItemDetails.parent.name
+        }
+        $TrashListOutput += $TrashItemOutput
+    }
 
-#>
+    Return $TrashListOutput
+}
+
+#Dev values
+$DebugPreference = 'Continue'
+$EmailDomain = "@baylor.edu"
+#$UserBearID = "joshua_ogden"
+#$TestUserID = "211497569" #joshua_ogden ID
+#$UserBearID = "Allen_Page"
+#$TestUserID = "12646907509" #allen_page ID
+#$UserBearID = "Chelsea_Lin1"
+#$TestUserID = "3916309915" #chelsea_lin1 ID
+$UserBearID = "Brad_Hodges"
+$TestUserID = "203020963" #Brad_Hodges ID
+$SharedOwnedFolderID = "51629651991" #Folder name: test
+$SharedFolderID = "8342900509" #Folder name: Application Services Group
+$NonsharedFolderID = "1802641245" #Folder name: Grad School
+$SharedOwnedFileID = "" #File name: 
+$SharedFileID = "" #File name: 
+$NonsharedFileID = "" #File name: 
+
+#$BoxUserTrash = Get-BoxUserTrashList -BoxUserID $TestUserID -UserBearID $UserBearID
+#$BoxUserTrash
+
+$BoxOwnedFolders = Get-BoxUserFolders -UserBearID $UserBearID -BoxUserID $TestUserID
+$BoxOwnedFolders
